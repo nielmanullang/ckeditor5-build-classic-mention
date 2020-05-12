9ebc5e9ad7da9d2f96db29344418c6c1db3930de
Daniel Manullang <danielthmanullang@gmail.com>
2020-05-12 15:53:32 +0700

    hide not use

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -10,24 +10,14 @@ import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
 import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote";
 import CKFinder from "@ckeditor/ckeditor5-ckfinder/src/ckfinder";
-import EasyImage from "@ckeditor/ckeditor5-easy-image/src/easyimage";
 import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-import Image from "@ckeditor/ckeditor5-image/src/image";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar";
-import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent";
 import Link from "@ckeditor/ckeditor5-link/src/link";
 import List from "@ckeditor/ckeditor5-list/src/list";
-import MediaEmbed from "@ckeditor/ckeditor5-media-embed/src/mediaembed";
 import Mention from "@ckeditor/ckeditor5-mention/src/mention";
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-import Table from "@ckeditor/ckeditor5-table/src/table";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
 
 export default class ClassicEditor extends ClassicEditorBase {}
@@ -41,22 +31,12 @@ ClassicEditor.builtinPlugins = [
 	Italic,
 	BlockQuote,
 	CKFinder,
-	EasyImage,
 	Heading,
-	Image,
-	ImageCaption,
-	ImageStyle,
-	ImageToolbar,
-	ImageUpload,
-	Indent,
 	Link,
 	List,
-	MediaEmbed,
 	Mention,
 	Paragraph,
 	PasteFromOffice,
-	Table,
-	TableToolbar,
 	TextTransformation,
 ];
 
@@ -72,27 +52,10 @@ ClassicEditor.defaultConfig = {
 			"bulletedList",
 			"numberedList",
 			"|",
-			"indent",
-			"outdent",
-			"|",
-			"imageUpload",
 			"blockQuote",
-			"insertTable",
-			"mediaEmbed",
 			"undo",
 			"redo",
 		],
-	},
-	image: {
-		toolbar: [
-			"imageStyle:full",
-			"imageStyle:side",
-			"|",
-			"imageTextAlternative",
-		],
-	},
-	table: {
-		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: "en",
